d12d05da4af8c4e50e7447142d312607787290ee
ZlAbdessamed23 <zallaabdessamed29@gmail.com>
2024-11-30 16:21:48 +0100

    second vps

--- src/app/api/auth/email-verification/[token]/controller.ts
+++ src/app/api/auth/email-verification/[token]/controller.ts
@@ -51,6 +51,6 @@ export function handleVerificationResult(
         400
       );
     case "success":
-      return NextResponse.redirect(new URL("/login", "http://104.154.75.47"));
+      return NextResponse.redirect(new URL("/login", "http://10.128.0.5"));
   }
 }
